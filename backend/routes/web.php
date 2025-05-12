48829b22decb37f7b1a848b2d60a146d5cf5e88a
Irving Gomez <gomirv.work@gmail.com>
2025-05-11 22:35:18 -0700

    remove

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Modules\Core\Actions\QuvelWelcome;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', QuvelWelcome::class)->name('welcome');
-Route::get('_', static function (): RedirectResponse|string {
-    if (app()->environment('production')) {
-        return redirect('https://quvel.127.0.0.1.nip.io');
-    }
-
-    return 'You are here because you are not logged in. Production will redirect you to the frontend.';
-})->name('login');
