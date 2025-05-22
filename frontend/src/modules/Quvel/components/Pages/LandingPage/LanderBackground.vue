9218c52e31503b9e091a2ee51c4678a8c1cc4246
Irving Gomez <gomirv.work@gmail.com>
2025-05-21 22:33:30 -0700

    lighten up landing animations for smaller devices

--- frontend/src/modules/Quvel/components/Pages/LandingPage/LanderBackground.vue
+++ frontend/src/modules/Quvel/components/Pages/LandingPage/LanderBackground.vue
@@ -4,48 +4,4 @@
 
   <!-- Purple blob -->
   <div class="blob-purple"></div>
-
-  <!-- Animated particles -->
-  <div class="particles">
-    <div
-      class="particle"
-      style="top: 15%; left: 10%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 25%; left: 30%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 60%; left: 15%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 40%; left: 70%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 75%; left: 80%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 85%; left: 40%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 10%; left: 60%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 50%; left: 50%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 30%; left: 85%"
-    ></div>
-    <div
-      class="particle"
-      style="top: 70%; left: 25%"
-    ></div>
-  </div>
 </template>
