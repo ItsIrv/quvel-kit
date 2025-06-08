1db6bb787f9058be5c1dddba21e0a1d02874c66c
Irving Gomez <gomirv.work@gmail.com>
2025-06-08 13:11:40 -0700

    derp

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Modules\Tenant\Providers\TenantMiddlewareProvider;
 use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,14 +17,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         TenantMiddlewareProvider::bootstrapMiddleware($middleware);
 
-        \Log::info('TRUST_PROXIES', [env('TRUST_PROXIES', false)]);
+        Log::info('TRUST_PROXIES', [env('TRUST_PROXIES', false)]);
 
         // Trust proxy headers when enabled via environment
         if (env('TRUST_PROXIES', false)) {
             $trustedProxies = env('TRUSTED_PROXY_IPS', '127.0.0.1,localhost');
             $proxyIps       = array_map('trim', explode(',', $trustedProxies));
 
-            \Log::info('TRUSTED_PROXY_IPS', [$proxyIps]);
+            Log::info('TRUSTED_PROXY_IPS', [$proxyIps]);
 
             $middleware->trustProxies(
                 $proxyIps,
